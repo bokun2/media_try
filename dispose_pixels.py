6a80ccfe70192ca29086aaf2242f981be93328e1
张博坤 <>
2023-05-27 01:56:53 +0800

    hhh

--- dispose_pixels.py
+++ dispose_pixels.py
@@ -1,16 +1,46 @@
-from moviepy.editor import VideoFileClip
+import cv2
 import os
-# Specify the input and output video file paths
-input_folder = "res"
-output_folder = "final"
-count=960
-left_crop = count
+input_folder = "hhh"
+output_folder = input_folder+"_final"
 
-# Load the video clip
+os.mkdir(output_folder)
 for root, dic, file in os.walk(input_folder):
-    for f in file:
+     for f in file:
         name=os.path.join(input_folder,f)
-        video = VideoFileClip(name)
-        right_crop = video.w - count
-        cropped_video = video.crop(x1=left_crop, x2=right_crop)
-        cropped_video.write_videofile(os.path.join(output_folder,f), codec="libx264", audio_codec="aac")
+        output_file=os.path.join(output_folder,f)
+        video = cv2.VideoCapture(name)
+
+        # Get the video's properties
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
+
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_file, fourcc, fps, (width-960*2, height))
+
+        # Read and write each frame of the video
+        while True:
+            ret, frame = video.read()
+
+            if not ret:
+                break
+
+            # Process the frame if needed
+
+            # For example, you can perform some operations on the frame using PyTorch
+            frame = frame[:, 960:(width - 960), :]
+
+            # Write the processed frame to the output video file
+            out.write(frame)
+
+        
+            # Exit if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release the video capture and writer objects
+        video.release()
+        out.release()
+
+
